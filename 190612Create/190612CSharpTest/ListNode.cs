6db9de3ee6b156b159540408c3fadabc7974c508
liuzhimu <liuzhimu2014@163.com>
2019-10-24 09:53:23 +0800

    PreCode
    PreCode

---
+++ 190612Create/190612CSharpTest/ListNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190612CSharpTest
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int InVal)
+        {
+            val = InVal;
+        }
+    }
+}
---
+++ 190612Create/190612CSharpTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _190612CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode tNode1 = new ListNode(2);
+            tNode1.next = new ListNode(4);
+            tNode1.next.next = new ListNode(3);
+            ListNode tNode2 = new ListNode(5);
+            tNode2.next = new ListNode(6);
+            tNode2.next.next = new ListNode(4);
+
+            while (tNode1 != null)
+            {
+                Console.WriteLine(tNode1.val);
+                tNode1 = tNode1.next;
+            }
+        }
+
+        //static ListNode AddTwoNumber(ListNode InNode1, ListNode InNode2)
+        //{
+                
+        //}
+    }
+}
