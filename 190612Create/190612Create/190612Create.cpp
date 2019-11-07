288764f178a215967d2c6b191185781a4c8ac905
liuzhimu <liuzhimu2014@163.com>
2019-11-07 22:54:53 +0800

    两数相加
    两数相加

--- 190612Create/190612Create/190612Create.cpp
+++ 190612Create/190612Create/190612Create.cpp
@@ -4,9 +4,104 @@
 #include "pch.h"
 #include <iostream>
 
+using namespace std;
+
+struct ListNode 
+{
+	int val;
+	ListNode* next;
+	ListNode(int x) : val(x), next(nullptr) {}
+};
+
+
+class Solution 
+{
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+	{
+		ListNode* tNumNode = new ListNode(0);
+		int tUnitNum = 0;
+		ListNode* tL1;
+		ListNode* tL2;
+		ListNode* tPoint;
+
+		tL1 = l1;
+		tL2 = l2;
+		tPoint = tNumNode;
+
+
+		do
+		{
+			int tSum = 0;
+			if (tL1 == nullptr && tL2 != nullptr) 
+			{
+				tSum = tL2->val + tUnitNum;
+				tL2 = tL2->next;
+			}
+			else if (tL1 != nullptr && tL2 == nullptr)
+			{
+				tSum = tL1->val + tUnitNum;
+				tL1 = tL1->next;
+			}
+			else 
+			{
+				tSum = tL1->val + tL2->val + tUnitNum;
+				tL1 = tL1->next;
+				tL2 = tL2->next;
+			}
+			
+			tUnitNum = 0;
+			if (tSum >= 10)
+			{
+				int tMod = tSum % 10;
+				tUnitNum = 1;
+				tPoint->val = tMod;
+			}
+			else 
+			{
+				tPoint->val = tSum;
+			}
+
+
+			if (tL1 != nullptr || tL2 != nullptr) 
+			{
+				ListNode* tNode = new ListNode(0);
+				tPoint->next = tNode;
+				tPoint = tPoint->next;
+			}
+			else if (tUnitNum == 1)
+			{
+				ListNode* tNode = new ListNode(1);
+				tPoint->next = tNode;
+			}
+
+		} while (tL1 != nullptr || tL2 != nullptr);
+
+		return tNumNode;
+	}
+};
+
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Solution* tSolution = new Solution();
+
+	ListNode* tListOne = new ListNode(3);
+	tListOne->next = new ListNode(4);
+	tListOne->next->next = new ListNode(6);
+
+	ListNode* tListTwo = new ListNode(5);
+	tListTwo->next = new ListNode(6);
+	tListTwo->next->next = new ListNode(4);
+
+	ListNode* tListResult;
+
+	tListResult = tSolution->addTwoNumbers(tListOne, tListTwo);
+
+	while (tListResult != nullptr)
+	{
+		cout << tListResult->val << endl;
+		tListResult = tListResult->next;
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
